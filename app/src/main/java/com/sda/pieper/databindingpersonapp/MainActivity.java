8b2ad4118d766c54e2f03e5928961df7f56bef0d
Adrian <adrpieper@gmail.com>
2017-01-19 19:21:36 +0100

    Dodałem przycisk czyszczący

--- app/src/main/java/com/sda/pieper/databindingpersonapp/MainActivity.java
+++ app/src/main/java/com/sda/pieper/databindingpersonapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.sda.pieper.databindingpersonapp;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
 
 import com.sda.pieper.databindingpersonapp.databinding.ActivityMainBinding;
 
@@ -12,10 +13,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Person person = new Person("Adrian", "Pieper", 1);
+        final Person person = new Person("Adrian", "Pieper", 1);
         binding.setPerson(person);
         person.setName("Adam");
 
+        binding.clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                person.setName("");
+                person.setSurname("");
+            }
+        });
 
     }
 }
