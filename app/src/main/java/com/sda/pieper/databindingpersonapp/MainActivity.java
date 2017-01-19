097288891e04593b57557bee3362506a1616eab6
Adrian <adrpieper@gmail.com>
2017-01-19 18:39:03 +0100

    Dodałem bindowanie

--- app/src/main/java/com/sda/pieper/databindingpersonapp/MainActivity.java
+++ app/src/main/java/com/sda/pieper/databindingpersonapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.sda.pieper.databindingpersonapp;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+
+import com.sda.pieper.databindingpersonapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setPerson(new Person("Adrian", "Pieper", 1));
+
     }
 }
